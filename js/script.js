9fbe933ea3fbcea5d2b790f674930a5f25420fe0
*Diogo <diogolimakas@hotmail.com>
2022-09-04 22:26:42 -0300

    modificação de métodos para setar operação, mudarEvento e atribuir valores

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // import Calcular from './calcular.js';
 import Calculate from './calcular.js';
 
-const calculate = new Calculate();
+const calculate = new Calculate('.itens-list li', '.result p');
 calculate.init()
